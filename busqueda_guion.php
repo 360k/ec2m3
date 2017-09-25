6d96fafda5537c50d656d08659189ab2fe98dcee
360k <360k.cat@gmail.com>
2017-09-25 18:37:07 +0200

    25/06/2017 H:18:36

---
+++ busqueda_guion.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "funcion_guion.php";
+
+
+use PHPUnit\Framework\TestCase;
+
+class DondeEstaelMedio extends TestCase{
+
+
+  public function testsecuenciaenmedio(){
+
+    $cadena = 'cms<->dkd';
+    $resultado=token($cadena);
+    $this->asserttrue($resultado);
+
+  }
+
+  public function testMedioaproxunodch(){
+    $cadena = "cmsd<->dkt";
+    $resultado=token($cadena);
+    $this->asserttrue($resultado);
+  }
+
+  public function testMedioaproxunoizq(){
+    $cadena = "cms<->zdkt";
+    $resultado=token($cadena);
+    $this->asserttrue($resultado);
+  }
+
+  public function testsinguion(){
+    $cadena = "cms<>dkd";
+    $resultado=token($cadena);
+    $this->assertfalse($resultado);
+  }
+
+
+  public function testsinelemntos(){
+    $cadena = "cmsdkd";
+    $resultado=token($cadena);
+    $this->assertfalse($resultado);
+  }
+
+
+  public function testsologuion(){
+    $cadena = "cms-dkd";
+    $resultado=token($cadena);
+    $this->assertfalse($resultado);
+  }
+
+
+}
+
+ ?>
--- trinity.php
+++ trinity.php
@@ -1,13 +1,42 @@
 <?php
-$cadena = 'asdfgfdde';
-$valor = 'df';
+$cadena = 'gjjhk<->fdde';
+$valor = '-';
+$mayorque = '>';
+$menorque = '<';
 
-$pos = strpos($cadena, $valor);
+$esta = strpos($cadena, $valor);
+$cun = strlen($cadena);
+$temporalmayorque = $esta + 1;
+$temporalmenorque = $esta - 1;
+$posmayorque = strpos($cadena, $mayorque);
+$posmenorque = strpos($cadena, $menorque);
 
-if ($pos !==false) {
+
+
+if ($esta !==false) {
   echo "La cadena ".$valor." existe";
+  echo "<br>";
+  echo "temporal menor que ".$temporalmenorque;
+  echo "<br>";
+  echo "temporal mayor que ".$temporalmayorque;
+  echo "<br>";
+  echo "posicion < ".$posmenorque." y la de > ".$posmayorque;
+  echo "<br>";
+  echo "La cadena tiene ".$cun." elementos";
+  echo "<br>";
+  echo "Tus elementos empiezan en en la posicion ".$esta;
+  $mitad = intval($cun/2);
+  echo "<br>";
+  echo "el valor de la mitad de la cadena es ".$mitad;
+  if (($esta == $mitad || $esta == $mitad-1 || $esta == $mitad+1) && ($temporalmenorque == $posmenorque) && ($temporalmayorque == $posmayorque) ){
+    echo "<br>";
+    echo "La cadena ".$valor. "Esta en medio";
+  }else{
+    echo "<br>";
+    echo "La cadena ".$valor. "no esta en medio";
+  }
 }else{
-  echo "La cadena ".$valor." no existe";
+  echo "La cadena ".$esta." no existe";
 }
 
  ?>
