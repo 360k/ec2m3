723315a243f1c6513e58cd353ae0600a01da0191
360k <360k.cat@gmail.com>
2017-09-27 19:48:35 +0200

    27/09/2017 H:19:48

--- src/arrays.php
+++ src/arrays.php
@@ -15,8 +15,8 @@ namespace Lib\arrays;
 
 /**
 *
-*función que busque el máximo de un array de enteros recibido como
-*argumento.
+*función que busca el valor maximo dentro de un array de enteros recibido.
+*
 *@example
 *
 *        1 Recorreremos el Array comprobando cual de los numeros que lo componen es el mayor.
@@ -31,9 +31,43 @@ namespace Lib\arrays;
 *@return string devuelve el numero mayor de los valores en el array
 */
 function max($array){
+
+    // TODO Resto documentar.
+
     return 0;
 }
 
+/**
+*
+*
+* funciones
+*
+*@author 360k <360k.cat@gmail.com>
+*
+*@license AGPL
+*
+*
+*/
+namespace Lib\arrays;
+
+/**
+*
+*función que busca el valor minimo dentro de un array de enteros recibido.
+*
+*@example
+*
+*        1 Recorreremos el Array comprobando cual de los numeros que lo componen es el mayor.
+*Teniendo en cuenta el signo del numero.
+*max([3,4,6,1,65,-3,6,10]): devolvera -3
+*
+*@example 1 Si todos fueran de signo negativo se tendra en cuenta el mayo de ellos.
+*max([-3,-14,-7-23]): devolvera -3
+*
+*@param string $array es donde recibimos el array
+*
+*@return string devuelve el numero mayor de los valores en el array
+*
+*/
 
 function min($array){
     return 0;
